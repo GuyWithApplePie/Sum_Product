03f8668fd417344d7544d8f8ac1477351f009d79
Nicholas Couzelis <Nicholas Couzelis>
2017-03-23 16:29:48 -0400

    Interface Complete

--- src/calculator/Frame.java
+++ src/calculator/Frame.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
 
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtInput;
+	private JTextField txtResult;
 
 	/**
 	 * Launch the application.
@@ -32,11 +38,29 @@ public class Frame extends JFrame {
 	 */
 	public Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 400, 130);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtInput = new JTextField();
+		txtInput.setToolTipText("Input");
+		txtInput.setBounds(10, 11, 254, 29);
+		contentPane.add(txtInput);
+		txtInput.setColumns(10);
+		
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.setToolTipText("Calculate");
+		btnCalculate.setBounds(274, 11, 100, 29);
+		contentPane.add(btnCalculate);
+		
+		txtResult = new JTextField();
+		txtResult.setText("Please type your input in the text field.");
+		txtResult.setToolTipText("Results");
+		txtResult.setEditable(false);
+		txtResult.setColumns(10);
+		txtResult.setBounds(10, 51, 364, 29);
+		contentPane.add(txtResult);
 	}
-
 }
